49fe1e3f8b16f52d179f9d7f6743b5fd5673fe11
sidd5sci <sidd5sci@gmail.com>
2017-02-17 15:12:19 +0530

    game of life
    game of life

---
+++ GameOfLife.py
@@ -0,0 +1,28 @@
+import pygame
+
+
+
+window = (600,400)
+pygame.init()
+pygame.display.set_mode(window)
+
+class physics:
+  def __init__():
+  self.pos = list()
+
+class creature(physics):
+  def __init__():
+    super.__init__()
+    self.body = []
+    self.velocty = vector()
+    self.health = 0.0 
+    self.egg = 0.0
+    
+
+def main():
+  pass
+  
+main()
+
+
+
