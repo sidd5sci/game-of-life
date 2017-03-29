cd8269312959436ccba4fa9230909c62990aeeaa
sidd5sci <sidd5sci@gmail.com>
2017-03-29 15:35:43 +0530

    Update timer.py

--- timer.py
+++ timer.py
@@ -8,6 +8,16 @@ class timer:
     def update(self):
         self.timer -= self.rate
         self.checkEnd()
-    def  checkEnd(self):
+    def checkEnd(self):
         if self.timer<- self.endAt:
             self.timer = self.endAt
+    def beginTimer(self,name='none' ,beg = 100.0,end= 0.0 ,rate = 5.0):
+        self.name = name
+        self.begAt = beg# start condition of the timer
+        self.timer = beg # timer
+        self.endAt = end # end condition of the timer
+        self.rate = rate # rate of change of timer
+    def setEnd(self,end):
+        self.endAt = end
+    def begAt(self,end):
+        self.endAt = end
