7e523b9380c484049e194765c2be8bed9e8f7f23
sidd5sci <sidd5sci@gmail.com>
2017-03-29 15:33:07 +0530

    Create timer.py

---
+++ timer.py
@@ -0,0 +1,13 @@
+class timer:
+    def __init__(self,name='none' ,beg = 100.0,end= 0.0 ,rate = 5.0)
+        self.name = name
+        self.begAt = beg# start condition of the timer
+        self.timer = beg # timer
+        self.endAt = end # end condition of the timer
+        self.rate = rate # rate of change of timer
+    def update(self):
+        self.timer -= self.rate
+        self.checkEnd()
+    def  checkEnd(self):
+        if self.timer<- self.endAt:
+            self.timer = self.endAt
